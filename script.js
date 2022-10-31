b7caf8d776c09e1bf48ef254417ca0eda6d437e2
Jung In <jungincha923@gmail.com>
2022-11-01 00:11:26 +1000

    update form

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const inputType = document.querySelector('.form_input-type');
 const inputReps = document.querySelector('.form_input-reps')
 const inputDuration = document.querySelector('.form_input-duration');
 const inputGrade = document.querySelector('.form_input-grade');
+let map;
+let mapEvent;
+let shoeIcon;
 
 navigator.geolocation.getCurrentPosition(function(position) {
     const {latitude} = position.coords;
@@ -15,9 +18,9 @@ navigator.geolocation.getCurrentPosition(function(position) {
     
     const coords = [latitude, longitude];
 
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
-    const shoeIcon = L.icon({
+    shoeIcon = L.icon({
         iconUrl: './image/climbing-shoes.png',
         iconSize: [30, 30], 
         iconAnchor: [22, 94],
@@ -28,12 +31,42 @@ navigator.geolocation.getCurrentPosition(function(position) {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords, {icon: shoeIcon}).addTo(map)
-    .bindPopup('Climb')
-    .openPopup();
+    // Handling clicks on map
+    map.on('click', function(mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputReps.focus();
+        
+    })
+    
 }, function() {
     alert('Could not get your position');
-})
+});
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Clear input fields
+    inputReps.value = inputDuration.value = inputGrade.value = '';
+
+    console.log(mapEvent);
+    const {lat, lng} = mapEvent.latlng;
+    
+    // Display marker
+    L.marker([lat, lng], {icon: shoeIcon})
+        .addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'climbing-popup',
+        }))
+        .setPopupContent('Climb')
+        .openPopup();
+});
+
+
 
 // class App {
 //     #map;
